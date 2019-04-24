d862425b64494d94dad24b9185cd17dd2cb76e89
ranccm <ranimnapier@gmail.com>
2019-04-24 00:37:08 -0700

    callbacks and array complete

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,4 +1,7 @@
-// Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
+// Create a higher order function and invoke the callback function to test your work. 
+// You have been provided an example of a problem and a solution to see how this works 
+// with our items array.  Study both the problem and the solution to figure out the rest of the 
+// problems.
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
@@ -27,24 +30,56 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
+
 }
+
+getLength(items, function(length) {
+  console.log(length);
+});
+
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length-1]);
 }
+
+last(items, function(last) {
+  console.log(last);
+});
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);
 }
+
+sumNums(2, 3, function(sum) {
+  console.log(sum);
+});
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y);
 }
 
-function contains(item, list, cb) {
+multiplyNums (2, 3, function(product) {
+  console.log(product);
+});
+
+function contains(thing, arr, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  if (arr.includes(thing)) {
+    return cb(true);
+  }
+  else {
+    return cb(false);
+  }
 }
+
+contains ('Pencil', items, function(whatever) {
+  console.log(whatever);
+});
 
 /* STRETCH PROBLEM */
 
